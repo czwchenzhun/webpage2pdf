b3ee1a03da4123f1407d7559fd92de498cb9000f
czwchenzhun <863657500@qq.com>
2019-07-06 12:16:24 +0800

    update

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
-# webpage2pdf
+#webpage2pdf
 
-This is a simple wrapper of the QWebengineview(PyQt5) class.
-It's base on the client/sever model.
-You can create multiple process to render html and print to pdf easily.
+<pre>
+This is a simple wrapper of the QWebEngineView(PyQt5) class.
+It's base on the client/server model.
+</pre>
+<h1>Usage</h1>
+<div style="background-color:#f9f9f9;border:2px solid #d3d3d3;">
+	<pre>
+		#! -*- coding:utf-8 -*-
+		from webpage2pdf import RenderManager		
+		#must start from __main__
+		if __name__=='__main__':
+			rm=RenderManager()
+			rm.addRender(num=2,showUI=True)
+			rm.from_url('http://www.baidu.com/','0.pdf')
+			rm.from_html("<a>Hello World!</a>",'1.pdf')
+			#rm.from_localFile('html/test.html','3.pdf')
+			print('start.')
+			rm.waitFinish()
+			print('finish all.')		
+	</pre>
+</div>
--- usage.py
+++ usage.py
@@ -1,23 +1,16 @@
 #！-*- coding:utf-8 -*-
 from webpage2pdf import RenderManager
-import time
 
 if __name__=='__main__':
     rm=RenderManager()
-    rm.addRender(1,showUI=True)
+    rm.addRender(2,showUI=True)
     rm.from_url('http://www.hao123.com','0.pdf')
     rm.from_html("<a>Hello World!你好世界！</a>",'1.pdf')
-    #rm.from_localFile(r'Test.html','2.pdf')
-    time.sleep(5)
+    rm.from_url('http://www.sougou.com','2.pdf')
+    rm.from_url('http://www.bilibili.com','3.pdf')
+    rm.from_url('http://www.nowcoder.com','4.pdf')
+    rm.from_url('http://www.hao123.com','5.pdf')
+    #rm.from_localFile('css/test.html','6.pdf')
     print('start')
-    #taskNum用于记录任务的总数,自动增长
-    #从记录已经完成的任务的队列中取出，任务执行结果的描述信息
-    #取得一条记录代表着完成一个任务，taskNum -1，taskNum为0时代表全部执行完成
-    #terminateAll终结所有进程
-    #也可以调用terminate(num)来决定结束多少个进程
-    while rm.taskNum>0:
-        des=rm.queFinish.get(True)
-        print(des)
-        rm.taskNum-=1
-    rm.terminateAll()
+    rm.waitFinish() 
     print('finish all')
